0f2266716a278d0aa5f6165d3e6686aec30c14f0
Fei Yuan <yuan@nscl.msu.edu>
2017-04-11 02:23:46 -0400

    Add proto_vector

--- test.sh
+++ test.sh
@@ -2,9 +2,11 @@
 set -eux
 CFLAGS="-std=c99 -Wall -Wextra -Wconversion -pedantic"
 
-gcc -O3 -Wall -Wextra -Wconversion -pedantic ode.c ode_demo.c -lm
+objs='ode.c ode_demo.c proto_vector.c'
 
-clang -fsanitize=undefined $CFLAGS ode.c ode_demo.c -lm
+gcc -O3 -Wall -Wextra -Wconversion -pedantic $objs -lm
+
+clang -fsanitize=undefined $CFLAGS $objs -lm
 if ! ./a.out >ode_demo.out || ! diff ode_demo.out ode_demo.txt >/dev/null; then
     if [ "`wc -c ode_demo.out | cut -f 1 -d ' '`" -ne 0 ]; then
         git --no-pager diff --no-index ode_demo.out ode_demo.txt
@@ -13,5 +15,5 @@ if ! ./a.out >ode_demo.out || ! diff ode_demo.out ode_demo.txt >/dev/null; then
 fi
 
 valgrind --error-exitcode=1 -q ./a.out >/dev/null
-clang -fsanitize=memory $CFLAGS ode.c ode_demo.c -lm && ./a.out >/dev/null
-clang -fsanitize=address $CFLAGS ode.c ode_demo.c -lm && ./a.out >/dev/null
+clang -fsanitize=memory $CFLAGS $objs -lm && ./a.out >/dev/null
+clang -fsanitize=address $CFLAGS $objs -lm && ./a.out >/dev/null
